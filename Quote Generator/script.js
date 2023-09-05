223eb3024fc79691922375d05e46c445d5252981
NiranjanPatil <nirnjanpatil5434@gmail.com>
2023-09-05 20:17:04 +0530

    html and css and javascript[two] commit

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,20 +1,26 @@
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const tweeterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 async function randomQuote() {
     const response = await fetch('https://api.quotable.io/random')
-    const quote = await response.json()
+    var quote = await response.json()
 
 
     // Output the quote and author name
     console.log(quote.content)
     console.log(`- ${quote.author}`)
 
-    const quoteContainer = document.getElementById("quote-container");
-    const quoteText = document.getElementById("quote");
-    const authorText = document.getElementById("author");
-    const twitterBtn = document.getElementById("twitter");
-    const newQuoteBtn = document.getElementById("new-quote");
 
     quoteText.textContent = quote.content;
     authorText.textContent = quote.author;
 }
 randomQuote();
 
+function tweetOne() {
+    let link = `http://twitter.com/intent?text=${quote.content} -${quote.author}`;
+    window.open(link, '_blank');
+}
+
+tweeterBtn.addEventListener('click', tweetOne)
+newQuoteBtn.addEventListener('click', randomQuote)
